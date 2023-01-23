553262fd1ec206e3e2eef82350237ab34f4302d5
mariavlasov <maria.vlasovk@frontegg.com>
2023-01-23 16:14:59 +0200

    update docs

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -119,43 +119,43 @@ resource "frontegg_workspace" "example" {
     palette {
       error {
         contrast_text = "#eeeef0"
-        dark= "#ae402c"
-        light= "#FFEEEA"
-        main= "#E1583E"
+        dark          = "#ae402c"
+        light         = "#FFEEEA"
+        main          = "#E1583E"
       }
       info {
         contrast_text = "#eeeef0"
-        dark= "#3c6492"
-        light= "#E2EEF9"
-        main= "#5587C0"
+        dark          = "#3c6492"
+        light         = "#E2EEF9"
+        main          = "#5587C0"
       }
       primary {
-        active = "#278854"
+        active        = "#278854"
         contrast_text = "#eeeef0"
-        dark = "#36A76A"
-        hover = "#32A265"
-        light = "#A2E1BF"
-        main = "#43BB7A"
+        dark          = "#36A76A"
+        hover         = "#32A265"
+        light         = "#A2E1BF"
+        main          = "#43BB7A"
       }
       secondary {
-        active = "#E6ECF4"
+        active        = "#E6ECF4"
         contrast_text = "#eeeef0"
-        dark = "#E6ECF4"
-        hover = "#F0F3F8"
-        light = "#FBFBFC"
-        main = "#FBFBFC"
+        dark          = "#E6ECF4"
+        hover         = "#F0F3F8"
+        light         = "#FBFBFC"
+        main          = "#FBFBFC"
       }
       success {
         contrast_text = "#eeeef0"
-        dark = "#1d7c30"
-        light = "#E1F5E2"
-        main = "#2CA744"
+        dark          = "#1d7c30"
+        light         = "#E1F5E2"
+        main          = "#2CA744"
       }
       warning {
         contrast_text = "#eeeef0"
-        dark = "#EAE1C2"
-        light = "#F9F4E2"
-        main = "#A79D7B"
+        dark          = "#EAE1C2"
+        light         = "#F9F4E2"
+        main          = "#A79D7B"
       }
     }
   }
@@ -170,6 +170,7 @@ resource "frontegg_webhook" "example" {
   events = [
     "frontegg.user.authenticated"
   ]
+  tenant_id = "TenantId"
 }
 
 resource "frontegg_permission_category" "example" {
