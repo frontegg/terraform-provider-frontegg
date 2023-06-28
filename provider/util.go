88c14dd2a592d2dee31d91cb18abdeb308d77cc3
mariavlasov <maria.vlasovk@frontegg.com>
2023-06-28 14:49:20 +0300

    add environment headers util

--- provider/util.go
+++ provider/util.go
@@ -1,6 +1,10 @@
 package provider
 
-import "github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+import (
+	"net/http"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
 
 func stringSetToList(set *schema.Set) []string {
 	out := make([]string, 0, set.Len())
@@ -8,4 +12,14 @@ func stringSetToList(set *schema.Set) []string {
 		out = append(out, v.(string))
 	}
 	return out
+}
+
+func getEnvironmentHeaders(environment_id string) http.Header {
+	headers := http.Header{}
+	if environment_id != "" {
+		headers.Add("frontegg-environment-id", environment_id)
+	} else {
+		headers = nil
+	}
+	return headers
 }
