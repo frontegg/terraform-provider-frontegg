0902cd382708d5045c515be1a83b84a655ef89e3
razshlomo-frontegg <raz.shlomo@frontegg.com>
2025-07-21 14:37:10 +0300

    remove comment

--- provider/resource_frontegg_email_template.go
+++ provider/resource_frontegg_email_template.go
@@ -166,14 +166,6 @@ func resourceFronteggEmailTemplateUpdate(ctx context.Context, d *schema.Resource
 	clientHolder := meta.(*restclient.ClientHolder)
 	in := resourceFronteggEmailTemplateSerialize(d)
 
-	// Set defaults for fields that may be required by the API
-	// if in.RedirectURL == "" {
-	// 	in.RedirectURL = "http://disabled"
-	// }
-	// if in.SenderEmail == "" {
-	// 	in.SenderEmail = "hello@frontegg.com"
-	// }
-
 	if err := clientHolder.ApiClient.Post(ctx, fronteggEmailTemplateURL, in, nil); err != nil {
 		return diag.FromErr(err)
 	}
