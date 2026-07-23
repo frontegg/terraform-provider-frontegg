a3f96ed7600840874bf8590734c7dcdbd0e12a0a
dianaKhortiuk-frontegg <diana.khortiuk@frontegg.com>
2026-07-23 11:45:54 +0300

    Revert single-event cap on custom code prehook events

--- provider/resource_frontegg_custom_code_prehook.go
+++ provider/resource_frontegg_custom_code_prehook.go
@@ -79,9 +79,8 @@ func resourceFronteggCustomCodePrehook() *schema.Resource {
 				Default:     10,
 			},
 			"events": {
-				Description: "The name of the event to subscribe to. Custom code prehooks support exactly one event.",
+				Description: "The name of the event to subscribe to.",
 				Type:        schema.TypeSet,
-				MaxItems:    1,
 				Elem: &schema.Schema{
 					Type: schema.TypeString,
 				},
